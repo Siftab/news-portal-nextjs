f5aa7910add502bc260c620f5b8577e88f02e7c2
Siftab <siiffuuu@gmail.com>
2024-03-24 21:42:46 +0600

    categoryBAr initialized

--- src/components/ui/CategoriesList/page.jsx
+++ src/components/ui/CategoriesList/page.jsx
@@ -1,11 +1,24 @@
 import useCategoryList from "@/components/Hooks/useCategoryList";
-import { Box } from "@mui/material";
+import { Box, Button, Divider, Stack, Typography } from "@mui/material";
 import React from "react";
 
 const CategoriesList = async () => {
   const allCategoryLists = await useCategoryList();
   console.log(allCategoryLists);
-  return <Box>this is Category List</Box>;
+  return (
+    <Box className="mt-5 bg-gray-300 px-5 py-3">
+      <Typography variant="h6">All Categories</Typography>
+
+      <Divider />
+      <Stack rowGap={2}>
+        {allCategoryLists.map((item) => (
+          <Button variant="outlined" key={item.id}>
+            {item.title}
+          </Button>
+        ))}
+      </Stack>
+    </Box>
+  );
 };
 
 export default CategoriesList;
