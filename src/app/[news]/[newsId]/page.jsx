92d2f7f8ff8de20e45ef7c36998f0a7d1ca94b55
Siftab <siiffuuu@gmail.com>
2024-03-25 04:04:38 +0600

    deatils section completed

--- src/app/[news]/[newsId]/page.jsx
+++ src/app/[news]/[newsId]/page.jsx
@@ -1,5 +1,5 @@
 import useSpecificNews from "@/components/Hooks/useSpecificNews";
-import { Box, Container, Grid } from "@mui/material";
+import { Box, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import React from "react";
 
@@ -9,7 +9,7 @@ const DetailsNews = async ({ params }) => {
   return (
     <Box>
       {/* {params.newsId} */}
-      <Grid container spacing={2}>
+      <Grid container spacing={2} className="mt-3">
         <Grid item xs={6}>
           <Container>
             <Image
@@ -40,7 +40,27 @@ const DetailsNews = async ({ params }) => {
           </Container>
         </Grid>
         <Grid item xs={6}>
-          <Box>content</Box>
+          <Box>
+            <Typography variant="h5">{newsData.data.title}</Typography>
+            <Box className="flex items-center space-x-4 mt-5">
+              <Image
+                src={newsData.data.author.img}
+                alt="author img"
+                width={60}
+                height={60}
+                className="rounded-full"
+              ></Image>
+              <Typography variant="body1">
+                {newsData.data.author.name}
+              </Typography>
+              <Typography variant="body1">
+                Date:{newsData.data.author.published_date}
+              </Typography>
+            </Box>
+            <Typography variant="body1" className="whitespace-pre-line mt-7">
+              {newsData.data.details}
+            </Typography>
+          </Box>
         </Grid>
       </Grid>
     </Box>
